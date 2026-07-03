9aa17dba0283e8f6dd9d590421b4e14adbabfd56
Dhruvkalal90 <kalaldhruv79@gmail.com>
2026-07-03 09:49:17 +0530

    added sitemap,robot

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      // Replace with your real, deployed domain name
+      hostname: 'https://dhruvmarketing.vercel.app', 
+      
+      // List out all the paths/routes available in your SPA
+      dynamicRoutes: [
+        '/',
+        '/#services',
+        '/#contact',
+        '/#areas'
+      ]
+    })
   ],
 })
